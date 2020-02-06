9a1cc29aaf7d3a4c69d7a1d220aa14f7d542ee26
Emerith <isc_ramirez.j@tesvb.edu.mx>
2020-02-05 21:53:07 -0600

    id_alumno

--- app/Http/Controllers/actividades_alumnoController.php
+++ app/Http/Controllers/actividades_alumnoController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\gnral_alumnos;
 use App\Plan_asigna_evidencias;
 use Illuminate\Foundation\Auth\User;
@@ -11,50 +9,32 @@ use DB;
 use App\planasignaevidencias;
 class actividades_alumnoController extends Controller
 {
-
     public function index()
     {
         $id=Auth::user()->email;
-        //echo $id;
-
-        $consulta=DB::table('gnral_alumnos')
+        $datos=DB::table('plan_actividades')
+            ->join('plan_asigna_planeacion_actividad','plan_asigna_planeacion_actividad.id_plan_actividad','=','plan_actividades.id_plan_actividad')
+            ->join('plan_planeacion','plan_planeacion.id_planeacion','=','plan_asigna_planeacion_actividad.id_planeacion')
+            ->join('plan_asigna_planeacion_tutor','plan_asigna_planeacion_tutor.id_asigna_planaeacion_actividad','=',
+                'plan_asigna_planeacion_actividad.id_asigna_planeacion_actividad')
+            ->join('plan_asigna_evidencias','plan_asigna_evidencias.id_asigna_planeacion_tutor','=','plan_asigna_planeacion_tutor.id_asigna_planeacion_tutor')
+            ->join('exp_asigna_tutor','exp_asigna_tutor.id_asigna_tutor','=','plan_asigna_planeacion_tutor.id_asigna_tutor')
+            ->join('gnral_alumnos','gnral_alumnos.id_alumno','=','plan_asigna_evidencias.id_alumno')
             ->join('users', 'users.email', '=', 'gnral_alumnos.correo_al')
             ->where('users.email','=',$id)
-            -> select('gnral_alumnos.id_alumno')
+            -> select('plan_actividades.desc_actividad', 'plan_actividades.objetivo_actividad',
+                'fi_actividad', 'plan_actividades.ff_actividad')
             ->get();
-       // echo $consulta;
-
-
-        $datos=DB::select('SELECT DISTINCT desc_actividad, objetivo_actividad, fi_actividad, ff_actividad, estrategia, requiere_evidencia 
-                            from plan_actividades, plan_planeacion, plan_asigna_planeacion_actividad, plan_asigna_planeacion_tutor, exp_asigna_generacion, 
-                            exp_asigna_tutor, exp_asigna_alumnos 
-                            where id_alumno=10
-                            and plan_asigna_planeacion_actividad.id_planeacion=plan_planeacion.id_planeacion
-                            and plan_asigna_planeacion_actividad.id_plan_actividad=plan_actividades.id_plan_actividad
-                            and plan_asigna_planeacion_tutor.id_asigna_planaeacion_actividad=plan_asigna_planeacion_actividad.id_asigna_planeacion_actividad
-                            and plan_asigna_planeacion_tutor.id_asigna_tutor=exp_asigna_tutor.id_asigna_tutor');
-
-        $datos1=DB::select('SELECT DISTINCT  plan_asigna_planeacion_tutor.id_asigna_planeacion_tutor, id_evidencia, evidencia 
-           from plan_asigna_planeacion_tutor, plan_asigna_evidencias
-           where id_alumno=10
-           and plan_asigna_planeacion_tutor.id_asigna_planeacion_tutor=plan_asigna_evidencias.id_asigna_planeacion_tutor');
-
-        return view('actividades_alumno.actividades_alumno',compact("datos","datos1"));
-
+        return view('actividades_alumno.actividades_alumno',compact("datos"));
     }
-
-
     public function create()
     {
         //
     }
-
-
     public function store(Request $request)
     {
 
     }
-
     public function updateExp(Request $request)
     {
         //            ""=>
@@ -63,18 +43,14 @@ class actividades_alumnoController extends Controller
 
 
     }
-
     public function show($id)
     {
         //
     }
-
-
     public function edit($id)
     {
         //
     }
-
     public function cerrar()
     {
         //
@@ -83,13 +59,10 @@ class actividades_alumnoController extends Controller
 
     public function update(Request $request, $id)
     {
-
         $plan1 = Plan_asigna_evidencias::find($id);
         if($request->hasFile('evidencia'))
         {
-
             $file=$request->file('evidencia');
-
             $name=time().".".$file->getClientOriginalExtension();
             $plan1->evidencia = $name;
             $file->move(public_path().'/img/',$name);
@@ -97,15 +70,12 @@ class actividades_alumnoController extends Controller
                 $file=$request->file('evidencia');
           //  dd($file);
                 $name=time()."daat";
-
                 $file->move(public_path().'/img/',$name);
         }
         //$plan->evidencia = $request->evidencia;;
         $plan1->save();
         return redirect()->back();
     }
-
-
     public function destroy($id)
     {
         //
--- resources/views/actividades_alumno/actividades_alumno.blade.php
+++ resources/views/actividades_alumno/actividades_alumno.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.app')
 @section('content')
-
-
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <div class="container card">
-
         <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="primero" role="tabpanel" aria-labelledby="primero-tab">
                 <br>
@@ -18,89 +15,23 @@
                         <th>Objetivo</th>
                         <th>Fecha Inicio</th>
                         <th>Fecha Fin</th>
-                        <th>Estrategia</th>
                         <th>Evidencia</th>
+                        <th>Estrategia</th>
                     </tr>
-
                     @foreach ($datos as $plan)
-
                         <tr onmouseover="this.style.backgroundColor='#DBE7F3'" onmouseout="this.style.backgroundColor='white'">
-
                             <td>{{$plan->desc_actividad}}</td>
                             <td>{{$plan->objetivo_actividad}}</td>
                             <td>{{$plan->fi_actividad}}</td>
                             <td>{{$plan->ff_actividad}}</td>
-                            <td>{{$plan->estrategia}}</td>
-                            @foreach($datos1 as $plan1)
-                            @if($plan->requiere_evidencia==1)
-
-                                @if($plan1->evidencia==null)
-                                    <td>Evidencia no agregada</td>
-                                @else
-                                    <td><a href="{{url("/img/",$plan1->evidencia)}}" target="_blank">Evidencia</a></td>
-                                @endif
-                            @else
-
-                            @endif
-
-                            @if($plan->requiere_evidencia==1)
-                                <td>
-                                    <a class="btn btn-light" data-toggle="modal" data-target="#myModal_{{$plan1->id_evidencia}}_tar">
-                                        Subir Evidencia
-                                    </a>
-                                </td>
-                            @else
-                                <td>Esta actividad no requiere evidencia</td>
-                            @endif
-
+                            <td></td>
+                            <td></td>
                         </tr>
-                    @endforeach
                     @endforeach
                 </table>
             </div>
         </div>
-
-        @foreach($datos1 as $dato)
-            <div class="modal fade" id="myModal_{{$dato->id_evidencia}}_tar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Evidencia</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="{{route('actividad.update',$dato->id_evidencia)}}" method="post" enctype="multipart/form-data">
-                                @csrf
-                                @method('PUT')
-                                <div class="modal-body">
-                                    <div class="form-group col-md-12">
-                                        <input type="file" class="form-control" name="evidencia">
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <div align="center">
-                                        <button type="submit" class="btn" style="background: #e0e0e0">Enviar</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-
-
-
-
-
-
-
-
-
     </div>
-
 @endsection
 
 
