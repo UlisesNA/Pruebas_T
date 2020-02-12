c4a3bba00636a56c09fcc62a708b7d2696604cdc
UlisesNA <38225332+UlisesNA@users.noreply.github.com>
2020-02-12 08:56:11 -0600

    cambios

--- app/Http/Controllers/Coordina_carrController.php
+++ app/Http/Controllers/Coordina_carrController.php
@@ -2,15 +2,48 @@
 namespace App\Http\Controllers;
 use App\Asigna_planeacion;
 use App\AsignaTutor;
+use App\User;
 use Illuminate\Http\Request;
 use DB;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
 
 class Coordina_carrController extends Controller
 {
     public function index()
     {
+        $carreras=DB::select('select gnral_carreras.nombre,gnral_carreras.id_carrera from exp_asigna_coordinador
+            JOIN gnral_personales ON exp_asigna_coordinador.id_personal=gnral_personales.id_personal 
+            JOIN gnral_jefes_periodos ON exp_asigna_coordinador.id_jefe_periodo=gnral_jefes_periodos.id_jefe_periodo 
+            JOIN gnral_carreras ON gnral_carreras.id_carrera=gnral_jefes_periodos.id_carrera WHERE exp_asigna_coordinador.id_jefe_periodo 
+            IN (SELECT gnral_jefes_periodos.id_jefe_periodo from gnral_jefes_periodos where gnral_jefes_periodos.id_periodo='.Session::get('id_periodo').') AND 
+            exp_asigna_coordinador.deleted_at is null ORDER BY gnral_carreras.nombre ');
+        $datos=DB::select('SELECT gnral_carreras.id_carrera,gnral_carreras.nombre as carrera,exp_generacion.generacion,
+                            exp_asigna_generacion.grupo,plan_actividades.*,plan_asigna_planeacion_tutor.*,gnral_personales.nombre
+                            FROM gnral_carreras,gnral_jefes_periodos,exp_asigna_coordinador,exp_generacion,
+                            exp_asigna_generacion,gnral_periodos,plan_planeacion,plan_asigna_planeacion_actividad,plan_actividades,plan_asigna_planeacion_tutor,exp_asigna_tutor,gnral_personales
+                            WHERE gnral_carreras.id_carrera=gnral_jefes_periodos.id_carrera
+                            AND gnral_jefes_periodos.id_jefe_periodo=exp_asigna_coordinador.id_jefe_periodo
+                            AND exp_asigna_coordinador.deleted_at is null
+                            AND gnral_periodos.id_periodo=exp_asigna_generacion.id_generacion
+                            AND exp_asigna_generacion.id_jefe_periodo=gnral_jefes_periodos.id_jefe_periodo
+                            AND exp_asigna_generacion.deleted_at is null
+                            AND exp_generacion.id_generacion=exp_asigna_generacion.id_generacion
+                            AND plan_planeacion.id_generacion=exp_asigna_generacion.id_generacion
+                            AND plan_planeacion.id_planeacion=plan_asigna_planeacion_actividad.id_planeacion
+                            AND plan_asigna_planeacion_actividad.id_plan_actividad=plan_actividades.id_plan_actividad
+                            AND plan_actividades.deleted_at is null
+                            AND plan_asigna_planeacion_tutor.id_asigna_planeacion_actividad=plan_asigna_planeacion_actividad.id_asigna_planeacion_actividad
+                            AND plan_asigna_planeacion_tutor.id_asigna_tutor=exp_asigna_tutor.id_asigna_tutor
+                            AND exp_asigna_tutor.id_asigna_generacion=exp_asigna_generacion.id_asigna_generacion
+                            AND exp_asigna_tutor.id_jefe_periodo=gnral_jefes_periodos.id_jefe_periodo
+                            AND plan_asigna_planeacion_tutor.id_sugerencia=2
+                            AND exp_asigna_coordinador.id_personal=gnral_personales.id_personal
+                            AND gnral_personales.tipo_usuario='.Auth::user()->id.'
+                            AND exp_asigna_tutor.id_asigna_tutor=plan_asigna_planeacion_tutor.id_asigna_tutor');
 
-        return view('coordina_carrera.index');
+        //dd(Auth::user()->id);
+        return view('coordina_carrera.index',compact('carreras','datos'));
     }
     public function store(Request $request)
     {
@@ -26,5 +59,34 @@ class Coordina_carrController extends Controller
         $planea = Asigna_planeacion::find($id);
         $planea->delete();
         return redirect()->back();
+    }
+
+    public function generaciones()
+    {
+
+        $generaciones=DB::select('SELECT gnral_carreras.id_carrera,gnral_carreras.nombre,exp_generacion.generacion,
+                            exp_asigna_generacion.grupo,plan_actividades.*,plan_asigna_planeacion_tutor.*,gnral_personales.nombre
+                            FROM gnral_carreras,gnral_jefes_periodos,exp_asigna_coordinador,exp_generacion,
+                            exp_asigna_generacion,gnral_periodos,plan_planeacion,plan_asigna_planeacion_actividad,plan_actividades,plan_asigna_planeacion_tutor,exp_asigna_tutor,gnral_personales
+                            WHERE gnral_carreras.id_carrera=gnral_jefes_periodos.id_carrera
+                            AND gnral_jefes_periodos.id_jefe_periodo=exp_asigna_coordinador.id_jefe_periodo
+                            AND exp_asigna_coordinador.deleted_at is null
+                            AND gnral_periodos.id_periodo=exp_asigna_generacion.id_generacion
+                            AND exp_asigna_generacion.id_jefe_periodo=gnral_jefes_periodos.id_jefe_periodo
+                            AND exp_asigna_generacion.deleted_at is null
+                            AND exp_generacion.id_generacion=exp_asigna_generacion.id_generacion
+                            AND plan_planeacion.id_generacion=exp_asigna_generacion.id_generacion
+                            AND plan_planeacion.id_planeacion=plan_asigna_planeacion_actividad.id_planeacion
+                            AND plan_asigna_planeacion_actividad.id_plan_actividad=plan_actividades.id_plan_actividad
+                            AND plan_actividades.deleted_at is null
+                            AND plan_asigna_planeacion_tutor.id_asigna_planeacion_actividad=plan_asigna_planeacion_actividad.id_asigna_planeacion_actividad
+                            AND plan_asigna_planeacion_tutor.id_asigna_tutor=exp_asigna_tutor.id_asigna_tutor
+                            AND exp_asigna_tutor.id_asigna_generacion=exp_asigna_generacion.id_asigna_generacion
+                            AND exp_asigna_tutor.id_jefe_periodo=gnral_jefes_periodos.id_jefe_periodo
+                            AND plan_asigna_planeacion_tutor.id_sugerencia=2
+                            AND exp_asigna_coordinador.id_personal=gnral_personales.id_personal
+                            AND gnral_personales.tipo_usuario='.Auth::user()->id.'
+                            AND exp_asigna_tutor.id_asigna_tutor=plan_asigna_planeacion_tutor.id_asigna_tutor');
+        return $generaciones;
     }
 }
--- resources/views/coordina_carrera/index.blade.php
+++ resources/views/coordina_carrera/index.blade.php
@@ -1,151 +1,69 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card">
-                    <div class="card-header">Alumnos</div>
-                    <div class="card-body">
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item">
-                                <a class="nav-link" id="generacion-tab" data-toggle="tab" href="#generacion" role="tab"  aria-controls="generacion" aria-selected="false">Profesores</a>
-                            </li>
-                        </ul>
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade pt-4" id="generacion" role="tabpanel" aria-labelledby="generacion-tab">
-                                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                                    <li class="nav-item" v-for="gen in generacion">
-                                        <a class="nav-link border m-1" @click="borrarAlumno(gen.generacion)" :id="'pills-'+gen.generacion+'-tab'" data-toggle="pill" :href="'#pills-'+gen.generacion" role="tab" :aria-controls="'pills-'+gen.generacion" aria-selected="true">@{{ gen.generacion }}</a>
-                                    </li>
-                                </ul>
-                                <div class="tab-content" id="pills-tabContent">
-                                    <div class="tab-pane fade " v-for="gen in generacion" :id="'pills-'+gen.generacion" role="tabpanel" :aria-labelledby="'pills-'+gen.generacion+'-tab'">
-                                        <div class="row border-bottom">
-                                            <div class="col-12">
-                                                <div class="row pb-3 pl-3">
-                                                    <button type="button" class="btn btn-outline-success" @click="confirma(gen)"> <i class="fas fa-plus"></i> Crear grupo</button>
-                                                </div>
-                                            </div>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel='stylesheet' href='{{ asset('css/sweetalert2.min.css') }}' />
+    <script src="{{asset('js/sweetalert2.min.js')}}"></script>
+    <div class="container card">
+        <div class="row">
+            <div class="col-md-12">
+                <nav>
+                    <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                        @foreach($carreras as $dato)
+                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#target_{{$dato->id_carrera}}" role="tab" aria-controls="target_{{$dato->id_carrera}}" aria-selected="false">{{$dato->nombre}}</a>
+                        @endforeach
+                    </div>
+                </nav>
+                <div class="tab-content" id="nav-tabContent">
+                    <br>
+                    @foreach($carreras as $dato)
+                        <div class="tab-pane fade" id="target_{{$dato->id_carrera}}" role="tabpanel" aria-labelledby="nav-profile-tab">
+                            <div class="dropdown">
+                                @foreach($datos as $dato)
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Generacion {{$dato->generacion}}
+                                    </button>
+                                    @foreach($datos as $dato)
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="#target_{{$dato->grupo}}">Grupo {{$dato->grupo}}</a>
                                         </div>
-                                        <ul class="nav nav-pills mb-3" id="grupo-tab" role="tablist">
-                                            <li class="nav-item">
-                                                <a class="nav-link border m-1" @click="getAlumnosGeneracion(gen.generacion)" id="pills-generalgen-tab" data-toggle="pill" href="#generalgen" role="tab" aria-controls="'pills-generalgen" >General</a>
-                                            </li>
-                                            <li class="nav-item btn-group" v-for="grupo in gen.grupos">
-                                                <a class="nav-link border m-1 "  @click="getAlumnosGrupo(grupo.id_asigna_generacion)" :id="'pills-'+grupo.id_asigna_generacion+'-tab'" data-toggle="pill" :href="'#pills-'+grupo.id_asigna_generacion" role="tab" :aria-controls="'pills-'+grupo.id_asigna_generacion">Grupo @{{ grupo.grupo }}</a>
-                                                <a href="#"><i class="fas text-danger fa-times h4 pt-3" data-toggle="tooltip" data-placement="bottom" title="Eliminar Grupo" @click="ConfirmaBorrar(grupo)"></i></a></td>
-                                            </li>
-                                        </ul>
-                                        <div class="row border-bottom-1" v-if="clicgrupo">
-                                            <div class="col-12">
-                                                <div class="row pb-3 pl-3">
-                                                    <button type="button" class="btn btn-outline-success" @click="Agregar()"> <i class="fas fa-plus"></i>Asignar alumnos</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="tab-content" id="grupo-tabContent" v-if="(alumno.length>0)">
-                                            <div class="tableFixHead">
-                                                <table class="table">
-                                                    <thead>
-                                                    <th>Cuenta</th>
-                                                    <th>Nombre</th>
-                                                    <th></th>
-                                                    </thead>
-                                                    <tbody>
-                                                    <tr v-for="alu in alumno">
-                                                        <td>@{{alu.cuenta}}</td>
-                                                        <td>@{{alu.apaterno}} @{{alu.amaterno}} @{{alu.nombre}}</td>
-                                                        <td v-if="clicgrupo"><button class="btn btn-outline-danger" @click="ConfirmaAlumno(alu)"><i class="fas fa-times" data-toggle="tooltip" data-placement="bottom" title="Eliminar"></i></button></td>
-                                                    </tr>
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                        <div class="row" v-if="alumno.length==0 && clicgrupo==true">
-                                            <div class="col-12 border-danger">
-                                                <h5 class="font-weight-bold text-center alert alert-danger">No existen alumnos asignados al grupo</h5>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    @endforeach
 
-                                </div>
+                                @endforeach
+                                @foreach($datos as $dato)
+                                        <div class="" id="target_{{$dato->grupo}}">
+                                            <div class="container card">
+                                                <div class="row">
+                                                    <div class="col-md-12">
+                                                        <br>
+                                                        <div class="tab-content" id="nav-tabContent">
+                                                            <div class="tab-pane fade show active" id="primero" role="tabpanel" aria-labelledby="primero-tab">
+                                                                <br>
+                                                                <div class="form-group row">
+                                                                    <div class="col-sm-11" align="center"><h5>Planeación Generación {{$dato->generacion}}</h5></div>
+                                                                </div>
+                                                                <table class="table table-hover table-sm">
+                                                                    <tr>
+                                                                        <th>Fecha Inicio</th>
+                                                                        <th>Fecha Fin</th>
+                                                                        <th>Decripción Actividad</th>
+                                                                        <th>Objetivo</th>
+                                                                        <th>Sugerencia</th>
+                                                                        <th>Estrategia</th>
+                                                                    </tr>
+
+                                                                </table>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
-        @include('alumno.modalCrearGrupo')
-        @include('alumno.EliminarGrupo')
-        @include('alumno.AgregarAlumnos')
-        @include('alumno.EliminarAlumno')
-    </div>
     </div>
 @endsection
-<script src="{{asset('js/jquery.js')}}"></script>
-<script>
-    $(document).ready(function () {
-        $('#planea').click(function(){
-            var con= true;
-            var datos = $('#form-expe').serialize();
-            $.ajax({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                url: "coordina_carrera",
-                method: "POST",
-                dataType: "json",
-                data:datos,
-                success:function (data) {
-                    location.reload();
-                },
-                error:function(request,status,data)
-                {
-                    alert("Hubo un error al insertar el dato, intentelo de nuevo")
-                    console.log(request)
-                    console.log(status)
-                    console.log(data)
-                }
-            });
-        });
-    });
-    function myFunction() {
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("docente");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("Table");
-        tr = table.getElementsByTagName("tr");
-
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
-    function verificar() {
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("verifica");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("tabla_v");
-        tr = table.getElementsByTagName("tr");
-
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
-</script>
-
